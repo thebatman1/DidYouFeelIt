42f640a8fa2fc20f5e7caf0ead1b3e3c71e302c0
Mrinmay Mukherjee <Mrinmay Mukherjee>
2017-03-30 17:14:20 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.didyoufeelit;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.net.URL;
+
+/**
+ * Displays the perceived strength of a single earthquake event based on responses from people who
+ * felt the earthquake.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /** URL for earthquake data from the USGS dataset */
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Update the information displayed to the user.
+        new Earthquake().execute(USGS_REQUEST_URL);
+
+    }
+
+    /**
+     * Update the UI with the given earthquake information.
+     */
+    private void updateUi(Event earthquake) {
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(earthquake.title);
+
+        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+        tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
+
+        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+        magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class Earthquake extends AsyncTask<String , Void , Event> {
+
+        // Perform the HTTP request for earthquake data and process the response.
+
+
+        @Override
+        protected Event doInBackground(String... params) {
+            if (params.length < 1 || params[0] == null) {
+                return null;
+            }
+            return Utils.fetchEarthquakeData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            if (event == null) {
+                return;
+            }
+
+            updateUi(event);
+        }
+    }
+}
